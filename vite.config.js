d74864e9fe623642ca1240472a1a0056806313d3
Craftgirlsss <craftgirlsssshopping@gmail.com>
2025-06-13 10:26:44 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-export default {
-  root: '.',
-  build: {
-    outDir: 'dist',
-  }
-};
+// export default {
+//   root: '.',
+//   build: {
+//     outDir: 'threejs',
+//   }
+// };
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/threejs/', // Sesuaikan dengan nama subfolder Anda
+  // ... konfigurasi Vite lainnya
+});
